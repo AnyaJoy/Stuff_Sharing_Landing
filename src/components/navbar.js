aad06364076115d5834190b3a8a0f1db1c164a10
anyajoymedcase <anya.jo@medcase.health>
2023-03-24 03:01:11 +0300

    landing page

---
+++ src/components/navbar.js
@@ -0,0 +1,40 @@
+import Image from 'next/image'
+import Link from 'next/link';
+import React from 'react'
+import plant from '../assets/plant.png'
+
+const infoPages = [{ name: "MISSION", slug: "/mission" }, { name: "BLOG", slug: "/blog" }, { name: "CONTACT", slug: "/contact" } ];
+const techPages = [{ name: "SIGN UP", slug: "/signup" }, { name: "LOGIN", slug: "/login" }];
+
+function Navbar() {
+  return (
+    <div className='w-screen flex justify-between bg-white px-40 py-4 navbar'>
+        <div className='flex justify-center'>
+            <Link href="/" className='flex justify-center align-middle'>
+                <Image src={plant} alt='logo' className='h-8 w-8'/>
+                <div className='text-orange font-fairy-tale italic ml-1 -mt-0.5 text-2xl tracking-wide xy-auto font-thin'>Stuff Sharing</div>
+            </Link>
+            <div className='hidden md:contents lg:flex'>
+                {infoPages.map((page, index) => (
+                    <Link href={page.slug} key={page.name}>
+                        <span className='ml-4 cursor-pointer hover:text-grey transition duration-700 text-sm font-semibold tracking-wider'>
+                            {page.name}
+                        </span>
+                    </Link>
+                ))}
+            </div>
+        </div>
+        <div className='hidden md:contents lg:flex '>
+            {techPages.map((page, index) => (
+                <Link href={page.slug} key={page.name}>
+                    <span className='ml-4 cursor-pointer hover:text-grey transition duration-700 text-sm font-semibold tracking-wider'>
+                        {page.name}
+                    </span>
+                </Link>
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ tailwind.config.js
@@ -0,0 +1,62 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  plugins: [],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ["ui-sans-serif", "system-ui"],
+      serif: ["ui-serif", "Georgia"],
+      mono: ["ui-monospace", "SFMono-Regular"],
+      display: ["Oswald"],
+      body: ['"Open Sans"'],
+      quote: ["Tangerine"],
+      "fairy-tale": ["Cormorant Upright"],
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      main: '#7C4DFF',
+      secondary: '#6200EA',
+      red: '#FF0000',
+      neutralGrey: '#B5B9CE',
+      neutral: {
+        500: "#8A8B96",
+        700: "#3B3B45",
+      },
+      'white': '#ffffff',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ececec',
+      'bubble-gum': '#ff77e9',
+      'brown': '#705746',
+      'grey': "#616161",
+      'purple-light': '#B388FF',
+      'purple': '#7C4DFF',
+      'blue-purple': '#651FFF',
+      'purple-dark': '#6200EA',
+      'purpleBlue': '#8C93FB',
+      'orange': '#ee6002',
+      'orange-light': '#FDBA74',
+      'orange-dark': '#B45309',
+      'orange-bg': '#FB923C',
+    },
+    screens: {
+      'sm': '600px',
+      // => @media (min-width: 600px) { ... }
+
+      'md': '900px',
+      // => @media (min-width: 900px) { ... }
+
+      'lg': '1200px',
+      // => @media (min-width: 1200px) { ... }
+
+      'xl': '1440px',
+      // => @media (min-width: 1440px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
+  }
+}
